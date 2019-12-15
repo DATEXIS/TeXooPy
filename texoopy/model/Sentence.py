e8e35364513a389c1f1a98cca589b22a042583e3
tkBischoff <tkbischoff@web.de>
2019-12-15 13:29:51 +0100

    Implemented to_json and from_json for Sentence(sentence splitting needs to be implemented still)

--- texoopy/model/Sentence.py
+++ texoopy/model/Sentence.py
@@ -3,15 +3,23 @@ from texoopy.model.Span import Span
 
 class Sentence(Span):
     def __init__(self, **kwargs):
-        # TODO IMPLEMENT ME
         super().__init__(**kwargs)
+        self.tokens: list = kwargs.get('tokens', [])
+        self.empty: bool  = kwargs.get('empty')
 
     @classmethod
     def from_json(cls, json_data: dict, do_tokenization=False):
         if do_tokenization:
             pass  # TODO implement me (later)
-
-        return cls(**json_data)  # TODO IMPLEMENT ME
+        token_json_data = json_data.pop('tokens', [])
+        sentence = cls(**json_data)
+        for json_token in token_json_data:
+            sentence.tokens.append(Token.from_json(json_token))
+        return sentence
 
     def to_json(self):
-        pass  # TODO implement me after there is a test for me
+        # TODO implement a test for me
+        return json.dumps(self.to_texoo_dict(), default = lambda o:o.to_texoo_dict())
+        
+    def to_texoo_dict(self):
+        return super().to_texoo_dict()
