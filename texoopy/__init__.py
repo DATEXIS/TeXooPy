f427d60967879a83782d1920756012aa61d6ebd7
tkBischoff <tkbischoff@web.de>
2020-02-07 14:25:10 +0100

    Fixed imports

--- texoopy/__init__.py
+++ texoopy/__init__.py
@@ -1,12 +1,11 @@
 """TeXooPy (texoopy) is a Python module that tackles the handling of TeXoo style JSON data."""
 
-from .model import (
-    Annotation,
-    Document,
-    Dataset,
-    MentionAnnotation,
-    NamedEntityAnnotation,
-    Sentence,
-    Span,
-    Token
-)
+
+from .model.Annotation import Annotation
+from .model.Document import Document
+from .model.Dataset import Dataset
+from .model.MentionAnnotation import MentionAnnotation
+from .model.NamedEntityAnnotation import NamedEntityAnnotation
+from .model.Sentence import Sentence
+from .model.Span import Span
+from .model.Token import Token
